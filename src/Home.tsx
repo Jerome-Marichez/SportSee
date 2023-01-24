a44c1bad821854a639e32ae379170dfdfc0e4d2e
Jerome Marichez <jeromemarichez@ik.me>
2023-01-24 14:46:30 +0100

    Create Title component

--- src/Home.tsx
+++ src/Home.tsx
@@ -9,29 +9,18 @@ import { lightTheme, darkTheme } from "./components/Themes"
 
 import Header from "./components/Header/Header";
 import SideBar from "./components/Sidebar/Siderbar";
+import Title from "./components/Title/Title";
 
 export default function Home() {
 	const [theme, themeToggler] = useDarkMode();
 
 	const DashboardContainer = styled.div`
-		position:absolute;
 		background-color: inherit;
 		margin-left: 224px; 
 		margin-top: 69px;
 		margin-right: 90px;
 		margin-bottom: 86px;
-	`;
-
-	const Title = styled.div`
-		font-family:  ${({ theme }) => theme.fontFamily};
-		font-style: bold;
-		font-weight: 600;
-		font-family:  ${({ theme }) => theme.fontWeight};
-		font-size: 48px;
-	`;
-
-	const TitleColor = styled(Title)`
-		color: ${({ theme }) => theme.colorThird};
+		
 	`;
 
 	return (
@@ -40,10 +29,13 @@ export default function Home() {
 				<GlobalStyles />
 				<Header />
 				<DashboardContainer>
-					<Title>Bonjour </Title><TitleColor>Thomas</TitleColor>
+					<Title text="Bonjour" textColor="Thomas" />
 				</DashboardContainer>
 				<SideBar />
-				<button onClick={themeToggler}>Switch Theme</button>
+				
+				{/* 
+				Create the button design at the end of the project
+				<button onClick={themeToggler}></button> */}
 			</>
 		</ThemeProvider>
 	);
---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Title({ text, textColor }) {
+
+
+	const H1flex = styled.h1`
+	display:flex;
+	gap: 15px;
+	`
+
+	const Title = styled.div`
+		font-family:  ${({ theme }) => theme.fontFamily};
+		font-style: bold;
+		font-weight: 600;
+		font-family:  ${({ theme }) => theme.fontWeight};
+		font-size: 48px;
+	`;
+
+	const TitleColor = styled(Title)`
+		color: ${({ theme }) => theme.colorThird};
+	`;
+
+	return (
+		<>
+			<H1flex>
+				<Title>{text}</Title><TitleColor>{textColor}</TitleColor>
+			</H1flex>
+		</>
+	);
+}
